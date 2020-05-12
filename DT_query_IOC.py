61fd45fef3720463c1cd3e2c261d4f443ca9cf3e
tomas.kabrt <tomas.kabrt@gmail.com>
2020-05-12 14:29:27 -0400

    Added dealing with long query field (DT have limit for 3k chars in advanced search)

--- DT_query_IOC.py
+++ DT_query_IOC.py
@@ -6,7 +6,21 @@ verbose = False
 DT_Queries = True
 fname = "ioc.txt"
 
-def fromat_text(title, item):
+
+def format_text(title, item):
+    if item:
+        cr = '\r\n'
+        section_break = cr + "*" * 20 + cr
+        title = str(title)
+        item = str(item)
+        text = Style.BRIGHT + Fore.GREEN + title + Fore.RESET + section_break + item + section_break
+    else:
+        text = ''
+    return text
+
+
+# format_query take in the account the 3k limit for advance search query lenght and devide the query into multiple queris if needed
+def format_query(title, item):
     if item:
         cr = '\r\n'
         section_break = cr + "*" * 20 + cr
@@ -20,13 +34,18 @@ def fromat_text(title, item):
 
 def generate_DT_query(iocs, source):
     query = ''
+    line_number = 1
 
     DT_query_mapping = {'ip': 'dest_ip:', 'md5': 'md5:', 'sha256': 'sha256:', 'domain': 'host:*'}
     for ioc in iocs[source]:
         if query == '':
             query = '@fields.%s%s' % (DT_query_mapping[source], ioc)
         else:
-            query += ' OR @fields.%s%s' % (DT_query_mapping[source], ioc)
+            if len(query) > line_number * 2800:
+                query += '\r\n@fields.%s%s' % (DT_query_mapping[source], ioc)
+                line_number += 1
+            else:
+                query += ' OR @fields.%s%s' % (DT_query_mapping[source], ioc)
 
     return query
 
@@ -38,15 +57,16 @@ def main():
     else:
         log.basicConfig(format="%(levelname)s: %(message)s")
 
-    ioc = IOC_file_extraction.main(["IOC_File_extraction", "%s" %fname])
+    ioc = IOC_file_extraction.main(["IOC_File_extraction", "%s" % fname])
 
     log.info(ioc)
 
     if DT_Queries:
-        print(fromat_text("Extracted IP addresses in the DT query:", generate_DT_query(ioc, "ip")))
-        print(fromat_text("Extracted Domain in the DT query:", generate_DT_query(ioc, "domain")))
-        print(fromat_text("Extracted MD5 hashes in the DT query:", generate_DT_query(ioc, "md5")))
-        print(fromat_text("Extracted SHA256 hashes in the DT query:", generate_DT_query(ioc, "sha256")))
+        print(format_text("Extracted IP addresses in the DT query:", generate_DT_query(ioc, "ip")))
+        print(format_text("Extracted Domain in the DT query:", generate_DT_query(ioc, "domain")))
+        print(format_text("Extracted MD5 hashes in the DT query:", generate_DT_query(ioc, "md5")))
+        print(format_text("Extracted SHA256 hashes in the DT query:", generate_DT_query(ioc, "sha256")))
+
 
 if __name__ == "__main__":
     main()
